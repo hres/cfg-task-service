67a272d6ccf83375efec67bf6cc1c77ad24bb637
rsanchej <raul.sanchez@canada.ca>
2017-02-17 13:47:38 -0500

    Adding code to get DB connection

---
+++ src/utils/DBConnection.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Properties;
+import java.sql.*;
+
+class DBConnection {
+    public static Connection getConnections() throws SQLException, Exception, IOException {
+
+        Connection conn = null;
+
+        Properties props = new Properties();
+        FileInputStream in = new FileInputStream("../../resources/db.properties");
+        props.load(in);
+        in.close();
+
+        String driver = props.getProperty("jdbc.driver");
+        if (driver == null) {
+            throw new Exception("jdbc.driver is not defined");
+        }
+
+        String url = props.getProperty("jdbc.url");
+        String username = props.getProperty("jdbc.username");
+        String password = props.getProperty("jdbc.password");
+
+        conn = DriverManager.getConnection(url, username, password);
+        return conn;
+    }
+}
