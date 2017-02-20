e873c8cf9aaede6c8e28e43611d0270e29ea0fd5
rsanchej <raul.sanchez@canada.ca>
2017-02-20 07:36:20 -0500

    integrating db conn

--- src/main/java/ca/gc/ip346/util/DBConnection.java
+++ src/main/java/ca/gc/ip346/util/DBConnection.java
@@ -1,27 +1,29 @@
+package ca.gc.ip346.util;
+
 import java.io.*;
 import java.util.Properties;
 import java.sql.*;
 
 class DBConnection {
-    public static Connection getConnections() throws SQLException, Exception, IOException {
+	public static Connection getConnections() throws SQLException, Exception, IOException {
 
-        Connection conn = null;
+		Connection conn = null;
 
-        Properties props = new Properties();
-        FileInputStream in = new FileInputStream("../../resources/db.properties");
-        props.load(in);
-        in.close();
+		Properties props = new Properties();
+		FileInputStream in = new FileInputStream("db.properties");
+		props.load(in);
+		in.close();
 
-        String driver = props.getProperty("jdbc.driver");
-        if (driver == null) {
-            throw new Exception("jdbc.driver is not defined");
-        }
+		String driver = props.getProperty("jdbc.driver");
+		if (driver == null) {
+			throw new Exception("jdbc.driver is not defined");
+		}
 
-        String url = props.getProperty("jdbc.url");
-        String username = props.getProperty("jdbc.username");
-        String password = props.getProperty("jdbc.password");
+		String url      = props.getProperty("jdbc.url");
+		String username = props.getProperty("jdbc.username");
+		String password = props.getProperty("jdbc.password");
 
-        conn = DriverManager.getConnection(url, username, password);
-        return conn;
-    }
+		conn = DriverManager.getConnection(url, username, password);
+		return conn;
+	}
 }
