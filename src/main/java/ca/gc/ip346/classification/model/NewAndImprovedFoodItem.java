20f2f49996ab2f86c913796ce2b2a24be6d61e4a
rsanchej <raul.sanchez@canada.ca>
2017-03-03 16:37:28 -0500

    new and improved food item

---
+++ src/main/java/ca/gc/ip346/classification/model/NewAndImprovedFoodItem.java
@@ -0,0 +1,726 @@
+package ca.gc.ip346.classification.model;
+
+import java.util.Date;
+
+public class NewAndImprovedFoodItem {
+	private double foodC;
+	private String engName;
+	private String frName;
+	private String foodDesc;
+	private String foodDescF;
+	private double nutCanC;
+	private Integer cnfFlag;
+	private Date dateEntry;
+	private Date dateChange;
+	private Date dateEnd;
+	private Date datePublic;
+	private String commentT;
+	private Integer groupC;
+	private Integer sourceC;
+	private Integer itemC;
+	private String countryC;
+	private String cnfRelease;
+	private String foodReference;
+	private String scientificName;
+	private String publicationFlag;
+	private String publicationCode;
+	private Integer sequenceC;
+	private double legacyGroupC;
+	private String fnCommentF;
+	private Integer fnDbSourceC;
+	private Integer fnRecipeFlg;
+	private Integer fnSystemViewC;
+	private double fnFatChange;
+	private double fnMoistureChange;
+	private Integer fnSysUserCreateC;
+	private Integer fnSysUserEditC;
+	private double fnTemplateC;
+	private Integer fnTemp;
+	private Date fnArchived;
+	private String fnLegacyC;
+	private double usRecipeC;
+	private Integer usdaModified;
+	private String usdaTemp;
+	private double canadaFoodSubgroupId;
+	private Integer cfgheFlag;
+	private double origCanadaFoodSubgroupId;
+	private Integer foodOwner;
+	private Integer sharedFood;
+	private String commonNmE;
+	private String commonNmF;
+	private String candiRecNum;
+	private Integer inheritanceFlag;
+	private double foodCode;
+
+	/**
+	 * @return the foodC
+	 */
+	public double getFoodC() {
+		return foodC;
+	}
+
+	/**
+	 * @param foodC the foodC to set
+	 */
+	public void setFoodC(double foodC) {
+		this.foodC = foodC;
+	}
+
+	/**
+	 * @return the engName
+	 */
+	public String getEngName() {
+		return engName;
+	}
+
+	/**
+	 * @param engName the engName to set
+	 */
+	public void setEngName(String engName) {
+		this.engName = engName;
+	}
+
+	/**
+	 * @return the frName
+	 */
+	public String getFrName() {
+		return frName;
+	}
+
+	/**
+	 * @param frName the frName to set
+	 */
+	public void setFrName(String frName) {
+		this.frName = frName;
+	}
+
+	/**
+	 * @return the foodDesc
+	 */
+	public String getFoodDesc() {
+		return foodDesc;
+	}
+
+	/**
+	 * @param foodDesc the foodDesc to set
+	 */
+	public void setFoodDesc(String foodDesc) {
+		this.foodDesc = foodDesc;
+	}
+
+	/**
+	 * @return the foodDescF
+	 */
+	public String getFoodDescF() {
+		return foodDescF;
+	}
+
+	/**
+	 * @param foodDescF the foodDescF to set
+	 */
+	public void setFoodDescF(String foodDescF) {
+		this.foodDescF = foodDescF;
+	}
+
+	/**
+	 * @return the nutCanC
+	 */
+	public double getNutCanC() {
+		return nutCanC;
+	}
+
+	/**
+	 * @param nutCanC the nutCanC to set
+	 */
+	public void setNutCanC(double nutCanC) {
+		this.nutCanC = nutCanC;
+	}
+
+	/**
+	 * @return the cnfFlag
+	 */
+	public Integer getCnfFlag() {
+		return cnfFlag;
+	}
+
+	/**
+	 * @param cnfFlag the cnfFlag to set
+	 */
+	public void setCnfFlag(Integer cnfFlag) {
+		this.cnfFlag = cnfFlag;
+	}
+
+	/**
+	 * @return the dateEntry
+	 */
+	public Date getDateEntry() {
+		return dateEntry;
+	}
+
+	/**
+	 * @param dateEntry the dateEntry to set
+	 */
+	public void setDateEntry(Date dateEntry) {
+		this.dateEntry = dateEntry;
+	}
+
+	/**
+	 * @return the dateChange
+	 */
+	public Date getDateChange() {
+		return dateChange;
+	}
+
+	/**
+	 * @param dateChange the dateChange to set
+	 */
+	public void setDateChange(Date dateChange) {
+		this.dateChange = dateChange;
+	}
+
+	/**
+	 * @return the dateEnd
+	 */
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+
+	/**
+	 * @param dateEnd the dateEnd to set
+	 */
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
+	}
+
+	/**
+	 * @return the datePublic
+	 */
+	public Date getDatePublic() {
+		return datePublic;
+	}
+
+	/**
+	 * @param datePublic the datePublic to set
+	 */
+	public void setDatePublic(Date datePublic) {
+		this.datePublic = datePublic;
+	}
+
+	/**
+	 * @return the commentT
+	 */
+	public String getCommentT() {
+		return commentT;
+	}
+
+	/**
+	 * @param commentT the commentT to set
+	 */
+	public void setCommentT(String commentT) {
+		this.commentT = commentT;
+	}
+
+	/**
+	 * @return the groupC
+	 */
+	public Integer getGroupC() {
+		return groupC;
+	}
+
+	/**
+	 * @param groupC the groupC to set
+	 */
+	public void setGroupC(Integer groupC) {
+		this.groupC = groupC;
+	}
+
+	/**
+	 * @return the sourceC
+	 */
+	public Integer getSourceC() {
+		return sourceC;
+	}
+
+	/**
+	 * @param sourceC the sourceC to set
+	 */
+	public void setSourceC(Integer sourceC) {
+		this.sourceC = sourceC;
+	}
+
+	/**
+	 * @return the itemC
+	 */
+	public Integer getItemC() {
+		return itemC;
+	}
+
+	/**
+	 * @param itemC the itemC to set
+	 */
+	public void setItemC(Integer itemC) {
+		this.itemC = itemC;
+	}
+
+	/**
+	 * @return the countryC
+	 */
+	public String getCountryC() {
+		return countryC;
+	}
+
+	/**
+	 * @param countryC the countryC to set
+	 */
+	public void setCountryC(String countryC) {
+		this.countryC = countryC;
+	}
+
+	/**
+	 * @return the cnfRelease
+	 */
+	public String getCnfRelease() {
+		return cnfRelease;
+	}
+
+	/**
+	 * @param cnfRelease the cnfRelease to set
+	 */
+	public void setCnfRelease(String cnfRelease) {
+		this.cnfRelease = cnfRelease;
+	}
+
+	/**
+	 * @return the foodReference
+	 */
+	public String getFoodReference() {
+		return foodReference;
+	}
+
+	/**
+	 * @param foodReference the foodReference to set
+	 */
+	public void setFoodReference(String foodReference) {
+		this.foodReference = foodReference;
+	}
+
+	/**
+	 * @return the scientificName
+	 */
+	public String getScientificName() {
+		return scientificName;
+	}
+
+	/**
+	 * @param scientificName the scientificName to set
+	 */
+	public void setScientificName(String scientificName) {
+		this.scientificName = scientificName;
+	}
+
+	/**
+	 * @return the publicationFlag
+	 */
+	public String getPublicationFlag() {
+		return publicationFlag;
+	}
+
+	/**
+	 * @param publicationFlag the publicationFlag to set
+	 */
+	public void setPublicationFlag(String publicationFlag) {
+		this.publicationFlag = publicationFlag;
+	}
+
+	/**
+	 * @return the publicationCode
+	 */
+	public String getPublicationCode() {
+		return publicationCode;
+	}
+
+	/**
+	 * @param publicationCode the publicationCode to set
+	 */
+	public void setPublicationCode(String publicationCode) {
+		this.publicationCode = publicationCode;
+	}
+
+	/**
+	 * @return the sequenceC
+	 */
+	public Integer getSequenceC() {
+		return sequenceC;
+	}
+
+	/**
+	 * @param sequenceC the sequenceC to set
+	 */
+	public void setSequenceC(Integer sequenceC) {
+		this.sequenceC = sequenceC;
+	}
+
+	/**
+	 * @return the legacyGroupC
+	 */
+	public double getLegacyGroupC() {
+		return legacyGroupC;
+	}
+
+	/**
+	 * @param legacyGroupC the legacyGroupC to set
+	 */
+	public void setLegacyGroupC(double legacyGroupC) {
+		this.legacyGroupC = legacyGroupC;
+	}
+
+	/**
+	 * @return the fnCommentF
+	 */
+	public String getFnCommentF() {
+		return fnCommentF;
+	}
+
+	/**
+	 * @param fnCommentF the fnCommentF to set
+	 */
+	public void setFnCommentF(String fnCommentF) {
+		this.fnCommentF = fnCommentF;
+	}
+
+	/**
+	 * @return the fnDbSourceC
+	 */
+	public Integer getFnDbSourceC() {
+		return fnDbSourceC;
+	}
+
+	/**
+	 * @param fnDbSourceC the fnDbSourceC to set
+	 */
+	public void setFnDbSourceC(Integer fnDbSourceC) {
+		this.fnDbSourceC = fnDbSourceC;
+	}
+
+	/**
+	 * @return the fnRecipeFlg
+	 */
+	public Integer getFnRecipeFlg() {
+		return fnRecipeFlg;
+	}
+
+	/**
+	 * @param fnRecipeFlg the fnRecipeFlg to set
+	 */
+	public void setFnRecipeFlg(Integer fnRecipeFlg) {
+		this.fnRecipeFlg = fnRecipeFlg;
+	}
+
+	/**
+	 * @return the fnSystemViewC
+	 */
+	public Integer getFnSystemViewC() {
+		return fnSystemViewC;
+	}
+
+	/**
+	 * @param fnSystemViewC the fnSystemViewC to set
+	 */
+	public void setFnSystemViewC(Integer fnSystemViewC) {
+		this.fnSystemViewC = fnSystemViewC;
+	}
+
+	/**
+	 * @return the fnFatChange
+	 */
+	public double getFnFatChange() {
+		return fnFatChange;
+	}
+
+	/**
+	 * @param fnFatChange the fnFatChange to set
+	 */
+	public void setFnFatChange(double fnFatChange) {
+		this.fnFatChange = fnFatChange;
+	}
+
+	/**
+	 * @return the fnMoistureChange
+	 */
+	public double getFnMoistureChange() {
+		return fnMoistureChange;
+	}
+
+	/**
+	 * @param fnMoistureChange the fnMoistureChange to set
+	 */
+	public void setFnMoistureChange(double fnMoistureChange) {
+		this.fnMoistureChange = fnMoistureChange;
+	}
+
+	/**
+	 * @return the fnSysUserCreateC
+	 */
+	public Integer getFnSysUserCreateC() {
+		return fnSysUserCreateC;
+	}
+
+	/**
+	 * @param fnSysUserCreateC the fnSysUserCreateC to set
+	 */
+	public void setFnSysUserCreateC(Integer fnSysUserCreateC) {
+		this.fnSysUserCreateC = fnSysUserCreateC;
+	}
+
+	/**
+	 * @return the fnSysUserEditC
+	 */
+	public Integer getFnSysUserEditC() {
+		return fnSysUserEditC;
+	}
+
+	/**
+	 * @param fnSysUserEditC the fnSysUserEditC to set
+	 */
+	public void setFnSysUserEditC(Integer fnSysUserEditC) {
+		this.fnSysUserEditC = fnSysUserEditC;
+	}
+
+	/**
+	 * @return the fnTemplateC
+	 */
+	public double getFnTemplateC() {
+		return fnTemplateC;
+	}
+
+	/**
+	 * @param fnTemplateC the fnTemplateC to set
+	 */
+	public void setFnTemplateC(double fnTemplateC) {
+		this.fnTemplateC = fnTemplateC;
+	}
+
+	/**
+	 * @return the fnTemp
+	 */
+	public Integer getFnTemp() {
+		return fnTemp;
+	}
+
+	/**
+	 * @param fnTemp the fnTemp to set
+	 */
+	public void setFnTemp(Integer fnTemp) {
+		this.fnTemp = fnTemp;
+	}
+
+	/**
+	 * @return the fnArchived
+	 */
+	public Date getFnArchived() {
+		return fnArchived;
+	}
+
+	/**
+	 * @param fnArchived the fnArchived to set
+	 */
+	public void setFnArchived(Date fnArchived) {
+		this.fnArchived = fnArchived;
+	}
+
+	/**
+	 * @return the fnLegacyC
+	 */
+	public String getFnLegacyC() {
+		return fnLegacyC;
+	}
+
+	/**
+	 * @param fnLegacyC the fnLegacyC to set
+	 */
+	public void setFnLegacyC(String fnLegacyC) {
+		this.fnLegacyC = fnLegacyC;
+	}
+
+	/**
+	 * @return the usRecipeC
+	 */
+	public double getUsRecipeC() {
+		return usRecipeC;
+	}
+
+	/**
+	 * @param usRecipeC the usRecipeC to set
+	 */
+	public void setUsRecipeC(double usRecipeC) {
+		this.usRecipeC = usRecipeC;
+	}
+
+	/**
+	 * @return the usdaModified
+	 */
+	public Integer getUsdaModified() {
+		return usdaModified;
+	}
+
+	/**
+	 * @param usdaModified the usdaModified to set
+	 */
+	public void setUsdaModified(Integer usdaModified) {
+		this.usdaModified = usdaModified;
+	}
+
+	/**
+	 * @return the usdaTemp
+	 */
+	public String getUsdaTemp() {
+		return usdaTemp;
+	}
+
+	/**
+	 * @param usdaTemp the usdaTemp to set
+	 */
+	public void setUsdaTemp(String usdaTemp) {
+		this.usdaTemp = usdaTemp;
+	}
+
+	/**
+	 * @return the canadaFoodSubgroupId
+	 */
+	public double getCanadaFoodSubgroupId() {
+		return canadaFoodSubgroupId;
+	}
+
+	/**
+	 * @param canadaFoodSubgroupId the canadaFoodSubgroupId to set
+	 */
+	public void setCanadaFoodSubgroupId(double canadaFoodSubgroupId) {
+		this.canadaFoodSubgroupId = canadaFoodSubgroupId;
+	}
+
+	/**
+	 * @return the cfgheFlag
+	 */
+	public Integer getCfgheFlag() {
+		return cfgheFlag;
+	}
+
+	/**
+	 * @param cfgheFlag the cfgheFlag to set
+	 */
+	public void setCfgheFlag(Integer cfgheFlag) {
+		this.cfgheFlag = cfgheFlag;
+	}
+
+	/**
+	 * @return the origCanadaFoodSubgroupId
+	 */
+	public double getOrigCanadaFoodSubgroupId() {
+		return origCanadaFoodSubgroupId;
+	}
+
+	/**
+	 * @param origCanadaFoodSubgroupId the origCanadaFoodSubgroupId to set
+	 */
+	public void setOrigCanadaFoodSubgroupId(double origCanadaFoodSubgroupId) {
+		this.origCanadaFoodSubgroupId = origCanadaFoodSubgroupId;
+	}
+
+	/**
+	 * @return the foodOwner
+	 */
+	public Integer getFoodOwner() {
+		return foodOwner;
+	}
+
+	/**
+	 * @param foodOwner the foodOwner to set
+	 */
+	public void setFoodOwner(Integer foodOwner) {
+		this.foodOwner = foodOwner;
+	}
+
+	/**
+	 * @return the sharedFood
+	 */
+	public Integer getSharedFood() {
+		return sharedFood;
+	}
+
+	/**
+	 * @param sharedFood the sharedFood to set
+	 */
+	public void setSharedFood(Integer sharedFood) {
+		this.sharedFood = sharedFood;
+	}
+
+	/**
+	 * @return the commonNmE
+	 */
+	public String getCommonNmE() {
+		return commonNmE;
+	}
+
+	/**
+	 * @param commonNmE the commonNmE to set
+	 */
+	public void setCommonNmE(String commonNmE) {
+		this.commonNmE = commonNmE;
+	}
+
+	/**
+	 * @return the commonNmF
+	 */
+	public String getCommonNmF() {
+		return commonNmF;
+	}
+
+	/**
+	 * @param commonNmF the commonNmF to set
+	 */
+	public void setCommonNmF(String commonNmF) {
+		this.commonNmF = commonNmF;
+	}
+
+	/**
+	 * @return the candiRecNum
+	 */
+	public String getCandiRecNum() {
+		return candiRecNum;
+	}
+
+	/**
+	 * @param candiRecNum the candiRecNum to set
+	 */
+	public void setCandiRecNum(String candiRecNum) {
+		this.candiRecNum = candiRecNum;
+	}
+
+	/**
+	 * @return the inheritanceFlag
+	 */
+	public Integer getInheritanceFlag() {
+		return inheritanceFlag;
+	}
+
+	/**
+	 * @param inheritanceFlag the inheritanceFlag to set
+	 */
+	public void setInheritanceFlag(Integer inheritanceFlag) {
+		this.inheritanceFlag = inheritanceFlag;
+	}
+
+	/**
+	 * @return the foodCode
+	 */
+	public double getFoodCode() {
+		return foodCode;
+	}
+
+	/**
+	 * @param foodCode the foodCode to set
+	 */
+	public void setFoodCode(double foodCode) {
+		this.foodCode = foodCode;
+	}
+}
