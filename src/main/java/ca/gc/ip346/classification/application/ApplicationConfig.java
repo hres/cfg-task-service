bb185332b596fb861bc9921c6b761b8886fcb106
rsanchej <raul.sanchez@canada.ca>
2017-07-12 14:55:10 -0400

    Created RulesResource class to house REST services Registered RulesResource class to load at startup

--- src/main/java/ca/gc/ip346/classification/application/ApplicationConfig.java
+++ src/main/java/ca/gc/ip346/classification/application/ApplicationConfig.java
@@ -12,50 +12,53 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/service")
 public class ApplicationConfig extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
+	@Override
+	public Set<Class<?>> getClasses() {
 
-        Set<Class<?>> resources = new HashSet<>();
+		Set<Class<?>> resources = new HashSet<>();
 
-        System.out.println("[01;34mREST configuration starting: getClasses()[00;00m");
+		System.out.println("[01;34mREST configuration starting: getClasses()[00;00m");
 
-        // features
-        // this will register Jackson JSON providers
-        resources.add(org.glassfish.jersey.jackson.JacksonFeature.class);
-        resources.add(com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider.class);
-        // or we can do it manually:
-        // resources.add( ca.gc.ip346.classification.provider.MyJacksonJsonProvider.class);
-		resources.add( ca.gc.ip346.classification.resource.FoodsResource.class);
-		resources.add( ca.gc.ip346.classification.resource.SubgroupsResource.class);
-        resources.add( ca.gc.ip346.classification.resource.GroupsResource.class);
-        resources.add( ca.gc.ip346.classification.resource.FlagsResource.class);
-        resources.add( ca.gc.ip346.classification.resource.TierAdjustmentsResource.class);
-        // ==> we could also choose packages, see below getProperties()
-        System.out.println("[01;34mFood Classification REST configuration ended successfully.[00;00m");
+		// features
+		// this will register Jackson JSON providers
+		resources.add(org.glassfish.jersey.jackson.JacksonFeature.class);
+		resources.add(com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider.class);
 
-        return resources;
-    }
+		// or we can do it manually:
+		// resources.add(ca.gc.ip346.classification.provider.MyJacksonJsonProvider.class);
+		resources.add(ca.gc.ip346.classification.resource.FoodsResource.class);
+		resources.add(ca.gc.ip346.classification.resource.SubgroupsResource.class);
+		resources.add(ca.gc.ip346.classification.resource.GroupsResource.class);
+		resources.add(ca.gc.ip346.classification.resource.FlagsResource.class);
+		resources.add(ca.gc.ip346.classification.resource.TierAdjustmentsResource.class);
+		resources.add(ca.gc.ip346.classification.resource.RulesResource.class);
 
-    @Override
-    public Set<Object> getSingletons() {
-        return Collections.emptySet();
-    }
+		// ==> we could also choose packages, see below getProperties()
+		System.out.println("[01;34mFood Classification REST configuration ended successfully.[00;00m");
 
-    @Override
-    public Map<String, Object> getProperties() {
-        Map<String, Object> properties = new HashMap<>();
+		return resources;
+	}
 
-        //in Jersey WADL generation is enabled by default, but we don't
-        //want to expose too much information about our apis.
-        //therefore we want to disable wadl (http://localhost:8080/service/application.wadl should return http 404)
-        //see https://jersey.java.net/nonav/documentation/latest/user-guide.html#d0e9020 for details
-        properties.put("jersey.config.server.wadl.disableWadl", true);
+	@Override
+	public Set<Object> getSingletons() {
+		return Collections.emptySet();
+	}
 
-        //we could also use something like this instead of adding each of our resources
-        //explicitly in getClasses():
-        //properties.put("jersey.config.server.provider.packages", "com.nabisoft.tutorials.mavenstruts.service");
+	@Override
+	public Map<String, Object> getProperties() {
+		Map<String, Object> properties = new HashMap<>();
+
+		//in Jersey WADL generation is enabled by default, but we don't
+		//want to expose too much information about our apis.
+		//therefore we want to disable wadl (http://localhost:8080/service/application.wadl should return http 404)
+		//see https://jersey.java.net/nonav/documentation/latest/user-guide.html#d0e9020 for details
+		properties.put("jersey.config.server.wadl.disableWadl", true);
+
+		//we could also use something like this instead of adding each of our resources
+		//explicitly in getClasses():
+		//properties.put("jersey.config.server.provider.packages", "com.nabisoft.tutorials.mavenstruts.service");
 
 
-        return properties;
-    }
+		return properties;
+	}
 }
--- src/main/java/ca/gc/ip346/classification/resource/RulesResource.java
+++ src/main/java/ca/gc/ip346/classification/resource/RulesResource.java
@@ -1,51 +1,87 @@
 package ca.gc.ip346.classification.resource;
 
+import static javax.ws.rs.HttpMethod.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Path("/rulesets")
+import com.google.gson.GsonBuilder;
 
+@Path("/rulesets")
+@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 public class RulesResource {
 	private static final Logger logger = LogManager.getLogger(RulesResource.class);
+	private Map<String, String> map = null;
 
 	public RulesResource() {
+		map = new HashMap<String, String>();
 	}
 
 	/**
-	 * Sprint  9 - Build a REST service to return rulesets
+	 * Sprint 5 - Build REST service to return rulesets
 	 */
 	@GET
-	public void getRules() {
+	public Response getRules() {
+		map.put("message", "REST service to return rulesets");
+		logger.error("\n[01;32m" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(map) + "[00;00m");
+		return FoodsResource.getResponse(GET, Response.Status.OK, map);
 	}
 
 	/**
-	 * Sprint 10 - Build a REST service to return a particular ruleset
+	 * Sprint 5 - Build REST service to return a particular ruleset
 	 */
 	@GET
 	@Path("id")
-	public void selectRules() {
+	public Response selectRules(@PathParam("id") String id) {
+		map.put("message", "REST service to return a particular ruleset");
+		logger.error("\n[01;32m" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(map) + "[00;00m");
+		return FoodsResource.getResponse(GET, Response.Status.OK, map);
 	}
 
 	/**
-	 * Sprint 11 - Build REST service to create new ruleset
-	 */
-	@PUT
-	public void createRules() {
-	}
-
-	/**
-	 * Sprint 12 - Build REST service to update and existing ruleset
+	 * Sprint 5 - Build REST service to create new ruleset
 	 */
 	@POST
+	public Response createRules() {
+		map.put("message", "REST service to create new ruleset");
+		logger.error("\n[01;32m" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(map) + "[00;00m");
+		return FoodsResource.getResponse(POST, Response.Status.OK, map);
+	}
+
+	/**
+	 * Sprint 5 - Build REST service to update an existing ruleset
+	 */
+	@PUT
 	@Path("id")
-	public void updateRules() {
+	public Response updateRules(@PathParam("id") String id) {
+		map.put("message", "REST service to update an existing ruleset");
+		logger.error("\n[01;32m" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(map) + "[00;00m");
+		return FoodsResource.getResponse(PUT, Response.Status.OK, map);
+	}
+
+	/**
+	 * Sprint 5 - Build REST service to delete an existing ruleset
+	 */
+	@DELETE
+	@Path("id")
+	public Response deleteRules(@PathParam("id") String id) {
+		map.put("message", "REST service to delete an existing ruleset");
+		logger.error("\n[01;32m" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(map) + "[00;00m");
+		return FoodsResource.getResponse(DELETE, Response.Status.OK, map);
 	}
 }
