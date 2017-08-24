7f1af67bf60fe1c44bd6c7f1c0664e6aca635e20
rsanchej <raul.sanchez@canada.ca>
2017-08-24 10:41:28 -0400

    * Add cross domain policy headers to all error responses e.g. 500 Internal Error * Fixed: Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at http://localhost:8080/cfg-task-service/service/datasets   - (Reason: CORS header Access-Control-Allow-Origin missing)

--- src/main/java/ca/gc/ip346/classification/resource/CORSFilter.java
+++ src/main/java/ca/gc/ip346/classification/resource/CORSFilter.java
@@ -17,13 +17,13 @@ public class CORSFilter implements ContainerResponseFilter {
 	private static final Logger logger = LogManager.getLogger(CORSFilter.class);
 
 	@Override
-	public void filter(final ContainerRequestContext requestContext,
-			final ContainerResponseContext cres) throws IOException {
-		cres.getHeaders().add("Access-Control-Allow-Origin", "*");
-		cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-		cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
-		cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-		cres.getHeaders().add("Access-Control-Max-Age", "1209600");;
-		logger.printf(DEBUG, "%s%s%s\n", "[01;03;33m", "Romario's Magic!!", "[00;00;00m");
+	public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) throws IOException {
+		responseContext.getHeaders().add("Access-Control-Allow-Origin",      "*");
+		responseContext.getHeaders().add("Access-Control-Allow-Headers",     "origin, content-type, accept, authorization");
+		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+		responseContext.getHeaders().add("Access-Control-Allow-Methods",     "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		responseContext.getHeaders().add("Access-Control-Max-Age",           "1209600");;
+
+		logger.printf(DEBUG, "%s%s%s\n", "[01;03;33m", "Romario's CORS Magic!!", "[00;00;00m");
 	}
 }
