8d629264ce19481ff5ce3462f2e6b610a0505d9e
rsanchej <raul.sanchez@canada.ca>
2017-08-25 14:34:27 -0400

    reverted back to development setting

--- src/main/java/ca/gc/ip346/classification/cors/CORSFilter.java
+++ src/main/java/ca/gc/ip346/classification/cors/CORSFilter.java
@@ -18,12 +18,12 @@ public class CORSFilter implements ContainerResponseFilter {
 
 	@Override
 	public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) throws IOException {
-		responseContext.getHeaders().add("Access-Control-Allow-Origin",      "https://lam-dev.hres.ca");
+		responseContext.getHeaders().add("Access-Control-Allow-Origin",      "*");
 		responseContext.getHeaders().add("Access-Control-Allow-Headers",     "ORIGIN, CONTENT-TYPE, ACCEPT, AUTHORIZATION, ACCESS_CONTROL_ALLOW_ORIGIN");
 		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
 		responseContext.getHeaders().add("Access-Control-Allow-Methods",     "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 		responseContext.getHeaders().add("Access-Control-Max-Age",           "1209600");;
 
-		logger.printf(DEBUG, "%s%s%s\n", "[01;03;33m", "Romario's CORS Magic!! - https://lam-dev.hres.ca", "[00;00;00m");
+		logger.printf(DEBUG, "%s%s%s\n", "[01;03;33m", "Romario's CORS Magic!! - *", "[00;00;00m");
 	}
 }
