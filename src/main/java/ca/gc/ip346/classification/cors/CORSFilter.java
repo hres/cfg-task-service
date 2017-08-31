f46867017ebca18737705e3c54cbd7cf23d9c09a
rsanchej <raul.sanchez@canada.ca>
2017-08-31 14:39:00 -0400

    CORS

--- src/main/java/ca/gc/ip346/classification/cors/CORSFilter.java
+++ src/main/java/ca/gc/ip346/classification/cors/CORSFilter.java
@@ -25,5 +25,6 @@ public class CORSFilter implements ContainerResponseFilter {
 		responseContext.getHeaders().add("Access-Control-Max-Age",           "1209600");;
 
 		logger.printf(DEBUG, "%s%s%s\n", "[01;03;33m", "Romario's CORS Magic!! - *", "[00;00;00m");
+		logger.printf(DEBUG, "%s%s%s\n", "[01;03;33m", "Romario's CORS Magic!! - *", "[00;00;00m");
 	}
 }
