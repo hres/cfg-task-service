8050ae588514bf00c1a95da85bf0c612e9b821c8
rsanchej <raul.sanchez@canada.ca>
2017-09-05 11:41:38 -0400

    Corrected ClientBuilder Response response-type

--- src/main/java/ca/gc/ip346/classification/resource/RulesResource.java
+++ src/main/java/ca/gc/ip346/classification/resource/RulesResource.java
@@ -5,6 +5,7 @@ import static javax.ws.rs.HttpMethod.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -14,6 +15,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -24,15 +26,15 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures;
 import com.google.gson.GsonBuilder;
 
-import ca.gc.ip346.util.ClassificationProperties;
-import ca.gc.ip346.util.RequestURI;
-
 @Path("/rulesets")
 @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 public class RulesResource {
 	private static final Logger logger = LogManager.getLogger(RulesResource.class);
 	private Map<String, String> map = null;
+
+	@Context
+	private HttpServletRequest request;
 
 	public RulesResource() {
 		map = new HashMap<String, String>();
@@ -45,17 +47,19 @@ public class RulesResource {
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@JacksonFeatures(serializationEnable = {SerializationFeature.INDENT_OUTPUT})
 	public Response getRules() {
+		String target = request.getRequestURL().toString().replaceAll("(\\w+:\\/\\/[^/]+(:\\d+)?/[^/]+).*", "$1").replaceAll("-task-", "-classification-");
 		map.put("message", "REST service to return rulesets");
 		logger.debug("\n[01;32m" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(map) + "[00;00m");
+		logger.debug("\n[01;32m" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(target) + "[00;00m");
 
 		Response response = ClientBuilder
 			.newClient()
-			.target(RequestURI.getUri() + ClassificationProperties.getEndPoint())
+			.target(target)
 			.path("/rulesets")
 			.request()
 			.get();
 
-		return FoodsResource.getResponse(GET, Response.Status.OK, response.getEntity());
+		return FoodsResource.getResponse(GET, Response.Status.OK, response.readEntity(Object.class));
 	}
 
 	/**
@@ -66,17 +70,18 @@ public class RulesResource {
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@JacksonFeatures(serializationEnable = {SerializationFeature.INDENT_OUTPUT})
 	public Response selectRules(@PathParam("id") String id) {
+		String target = request.getRequestURL().toString().replaceAll("(\\w+:\\/\\/[^/]+(:\\d+)?/[^/]+).*", "$1").replaceAll("-task-", "-classification-");
 		map.put("message", "REST service to return a particular ruleset");
 		logger.debug("\n[01;32m" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(map) + "[00;00m");
 
 		Response response = ClientBuilder
 			.newClient()
-			.target(RequestURI.getUri() + ClassificationProperties.getEndPoint())
+			.target(target)
 			.path("/rulesets/" + id)
 			.request()
 			.get();
 
-		return FoodsResource.getResponse(GET, Response.Status.OK, response.getEntity());
+		return FoodsResource.getResponse(GET, Response.Status.OK, response.readEntity(Object.class));
 	}
 
 	/**
@@ -108,16 +113,17 @@ public class RulesResource {
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@JacksonFeatures(serializationEnable = {SerializationFeature.INDENT_OUTPUT})
 	public Response deleteRules(@PathParam("id") String id) {
+		String target = request.getRequestURL().toString().replaceAll("(\\w+:\\/\\/[^/]+(:\\d+)?/[^/]+).*", "$1").replaceAll("-task-", "-classification-");
 		map.put("message", "REST service to delete an existing ruleset");
 		logger.debug("\n[01;32m" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(map) + "[00;00m");
 
 		Response response = ClientBuilder
 			.newClient()
-			.target(RequestURI.getUri() + ClassificationProperties.getEndPoint())
+			.target(target)
 			.path("/rulesets/" + id)
 			.request()
 			.delete();
 
-		return FoodsResource.getResponse(DELETE, Response.Status.OK, response.getEntity());
+		return FoodsResource.getResponse(DELETE, Response.Status.OK, response.readEntity(Object.class));
 	}
 }
