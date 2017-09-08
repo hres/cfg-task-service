db3851a734c45ff503f1693c1712688b1921c7f1
rsanchej <raul.sanchez@canada.ca>
2017-09-08 07:16:40 -0400

    refactored parameters

--- src/main/java/ca/gc/ip346/classification/cors/CORSFilter.java
+++ src/main/java/ca/gc/ip346/classification/cors/CORSFilter.java
@@ -17,13 +17,13 @@ public class CORSFilter implements ContainerResponseFilter {
 	private static final Logger logger = LogManager.getLogger(CORSFilter.class);
 
 	@Override
-	public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) throws IOException {
-		responseContext.getHeaders().add("Access-Control-Allow-Origin",      "*");
-		responseContext.getHeaders().add("Access-Control-Allow-Headers",     "ORIGIN, CONTENT-TYPE, ACCEPT, AUTHORIZATION, ACCESS_CONTROL_ALLOW_ORIGIN, X-REQUESTED-WITH");
-		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-		responseContext.getHeaders().add("Access-Control-Allow-Methods",     "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-		responseContext.getHeaders().add("Access-Control-Max-Age",           "1209600");
-		responseContext.getHeaders().add("Referrer-Policy",                  "no-referrer");
+	public void filter(final ContainerRequestContext request, final ContainerResponseContext response) throws IOException {
+		response.getHeaders().add("Access-Control-Allow-Origin",      "*");
+		response.getHeaders().add("Access-Control-Allow-Headers",     "ORIGIN, CONTENT-TYPE, ACCEPT, AUTHORIZATION, ACCESS_CONTROL_ALLOW_ORIGIN, X-REQUESTED-WITH");
+		response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+		response.getHeaders().add("Access-Control-Allow-Methods",     "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		response.getHeaders().add("Access-Control-Max-Age",           "1209600");
+		response.getHeaders().add("Referrer-Policy",                  "no-referrer");
 
 		logger.printf(DEBUG, "%s%s%s", "[01;03;33m", "Romario's CORS Magic!! - *", "[00;00;00m");
 		logger.printf(DEBUG, "%s%s%s", "[01;03;33m", "Romario's CORS Magic!! - *", "[00;00;00m");
